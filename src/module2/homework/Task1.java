7e18cd0071186953d50d7da4969068e6706ca935
roman-yarosh <yracompany@gmail.com>
2016-08-30 14:19:54 +0300

    Commit homework2 task 1

--- src/module2/homework/Task1.java
+++ src/module2/homework/Task1.java
@@ -31,7 +31,7 @@ public class Task1 {
         double min = balances[0];
         for (double balance : balances) {
             if (balance < min)
-            min = balance;
+                min = balance;
         }
         return min;
     }
@@ -72,6 +72,50 @@ public class Task1 {
         return max;
     }
 
+    static long multiplication(int[] balances) {
+        long mul = 1;
+        for (long balance : balances) {
+            mul *= balance;
+        }
+        return mul;
+    }
+
+    static double multiplication(double[] balances) {
+        double mul = 1.0;
+        for (double balance : balances) {
+            mul *= balance;
+        }
+        return mul;
+    }
+
+    static int modulus(int[] balances) {
+        return balances[0] % balances[balances.length - 1];
+    }
+
+    static double modulus(double[] balances) {
+        return balances[0] % balances[balances.length - 1];
+    }
+
+
+    static int secondLargest(int[] balances) {
+        int firstLargest = max(balances);
+        int secondLargest = balances[0];
+        for (int balance : balances) {
+            if (balance < firstLargest && balance > secondLargest)
+                secondLargest = balance;
+        }
+        return secondLargest;
+    }
+
+    static double secondLargest(double[] balances) {
+        double firstLargest = max(balances);
+        double secondLargest = balances[0];
+        for (double balance : balances) {
+            if (balance < firstLargest && balance > secondLargest)
+                secondLargest = balance;
+        }
+        return secondLargest;
+    }
 
     public static void main(String[] args) {
 
@@ -97,6 +141,34 @@ public class Task1 {
 
         System.out.print("\nint maxPositive: " + maxPositive(balances1));
         System.out.print("\ndouble maxPositive: " + maxPositive(balances2));
+
+        System.out.print("\nlong multiplication: " + multiplication(balances1));
+        System.out.print("\ndouble multiplication: " + multiplication(balances2));
+
+        System.out.print("\nint modulus: " + modulus(balances1));
+        System.out.print("\ndouble modulus: " + modulus(balances2));
+
+        System.out.print("\nint secondLargest: " + secondLargest(balances1));
+        System.out.print("\ndouble secondLargest: " + secondLargest(balances2));
+
+        /* Вывод результата в консоль
+            int array: 1200 250 2000 -500 3200 1600 2300 4500 305 110
+            double array: 1200.5 250.5 2000.5 -500.5 3200.5 1600.5 2300.5 4500.5 305.5 110.5
+            int sum: 14965
+            double sum: 14969.0
+            int min: -500
+            double min: -500.5
+            int max: 4500
+            double max: 4500.5
+            int maxPositive: 4500
+            double maxPositive: 4500.5
+            long multiplication: -2399349538394472448
+            double multiplication: -5.390667416805744E29
+            int modulus: 100
+            double modulus: 95.5
+            int secondLargest: 3200
+            double secondLargest: 3200.5
+         */
     }
 
 }
