f345fc2786a53cee5cd031c5d5cd39293fa3a9c7
roman-yarosh <yracompany@gmail.com>
2016-09-05 17:37:37 +0300

    Commit homework 3 tasks 1

---
+++ src/module3/homework/task1/Bird.java
@@ -0,0 +1,16 @@
+package module3.homework.task1;
+
+public class Bird extends Animal{
+
+    void fly() {
+        System.out.println("I am flying");
+    }
+
+    void sing() {
+        System.out.println("I am singing");
+    }
+
+    void whoAmI() {
+        System.out.println("I am Bird");
+    }
+}
