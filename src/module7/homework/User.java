49194983c4beb14bb80f744fa29e30e431ed4740
roman-yarosh <yracompany@gmail.com>
2016-10-08 18:57:36 +0300

    Commit homework 1 task 1

---
+++ src/module7/homework/User.java
@@ -0,0 +1,9 @@
+package module7.homework;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int balance;
+}
