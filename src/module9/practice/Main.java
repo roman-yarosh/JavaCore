698191dfa81e5813102be874eb93cfce24638d97
roman-yarosh <yracompany@gmail.com>
2016-10-19 12:31:13 +0300

    Commit homework 9

--- src/module9/practice/Main.java
+++ src/module9/practice/Main.java
@@ -1,7 +1,29 @@
 package module9.practice;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("");
+        List<String> names = Arrays.asList("Peter", "Anna", "Mike", "Kseniya");
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare (String a, String b) {
+                return b.compareTo(a);
+//                return a.compareTo(b);
+            }
+        });
+        System.out.println(names);
+
+        Collections.sort(names, (a, b) -> a.compareTo(b));
+
+        System.out.println(names);
+
+        Collections.sort(names, String::compareTo);
+
+        System.out.println(names);
+
     }
 }
